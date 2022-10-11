1e944223cf281f79e4a8b6954c2e9a44a47b3f38
zenyangwei <43901693+weizenyang@users.noreply.github.com>
2022-10-11 14:40:11 +0100

    idk waht to name this anymore

--- src/pages/Submissions.jsx
+++ src/pages/Submissions.jsx
@@ -107,10 +107,12 @@ export function SubNavbarJsx({id}) {
 										</h2>
 										<div className="downloads">
 											<a className="file">
+											<div class="info">
 												<img src="/pdf.png" alt="" />
 												<p>
 													The Permission and Release Form for Photography, Video and Broadcasting
 												</p>
+												</div>
 											</a>
 
 											<a className="file" href="https://www.springernature.com/gp/authors/campaigns/latex-author-support" target="_blank">
